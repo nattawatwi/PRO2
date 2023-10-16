c55fad1e810e65b934397dce12021ed0fa4c769a
nattawat <nattawat.wi@kkumail.com>
2023-10-16 16:22:57 +0700

    update

--- resources/views/frontend/pages/service.blade.php
+++ resources/views/frontend/pages/service.blade.php
@@ -27,28 +27,37 @@
 
 
    <!-- Service Start -->
-    <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
+   <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
         <div class="container py-5">
             <div class="section-title text-center position-relative pb-3 mb-5 mx-auto" style="max-width: 600px;">
-                <h5 class="fw-bold text-primary text-uppercase">Our Services</h5>
-                <h1 class="mb-0">Professional Services to Help Your Business</h1>
+                <h5 class="fw-bold text-primary text-uppercase">บริการของเรา</h5>
+                <h2 class="mb-0">บริการที่ดี มีประสิทธิภาพ ส่งเสริมการทำงาน เพื่อความสะดวกของคุณ</h2>
             </div>
             <div class="row g-5">
-            <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.3s">
+            @foreach ($services as $service)
+                <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.3s">
                     <div class="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
                         <div class="service-icon">
                             <i class="fa fa-shield-alt text-white"></i>
                         </div>
-                        <h4 class="mb-3">บริการ VPN</h4>
-                        <p class="m-0">Amet justo dolor lorem kasd amet magna sea stet eos vero lorem ipsum dolore sed</p>
-                        <a class="btn btn-lg btn-primary rounded" href="{{ url('pages/detail-service') }}">
+                        <h4 class="mb-3">บริการ VPN {{ $service->Sv_name }}</h4>
+                        <p class="m-0">{{ $service->description }}</p>
+                        <a class="btn btn-lg btn-primary rounded" href="{{ url('pages/detail-service/' . $service->id) }}">
                             <i class="bi bi-arrow-right"></i>
                         </a>
                     </div>
                 </div>
+            @endforeach
+            <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.9s">
+                <div class="position-relative bg-primary rounded h-100 d-flex flex-column align-items-center justify-content-center text-center p-5">
+                    <h3 class="text-white mb-3">Call Us For Quote</h3>
+                    <p class="text-white mb-3">Clita ipsum magna kasd rebum at ipsum amet dolor justo dolor est magna stet eirmod</p>
+                    <h2 class="text-white mb-0">+012 345 6789</h2>
+                </div>
             </div>
         </div>
     </div>
+
     <!-- Service End -->
 
     <!-- Back to Top -->
@@ -57,7 +66,6 @@
     <!-- Template Javascript -->
     <script src="{{asset('frontend/js/main.js')}}"></script>
 </body>
-
 </html>
 @endsection
 
