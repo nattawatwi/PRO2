afd6b4b10be66eaaf12494ef71d6e99e87e53a3c
Jeerasak07 <76503403+Jeerasak07@users.noreply.github.com>
2023-10-21 10:51:12 +0700

    Update

--- resources/views/frontend/pages/contact.blade.php
+++ resources/views/frontend/pages/contact.blade.php
@@ -40,7 +40,7 @@
                         </div>
                         <div class="ps-4">
                             <h5 class="mb-2">Call to ask any question</h5>
-                            <h4 class="text-primary mb-0">+012 345 6789</h4>
+                            <h4 class="text-primary mb-0">042-411500</h4>
                         </div>
                     </div>
                 </div>
@@ -51,7 +51,7 @@
                         </div>
                         <div class="ps-4">
                             <h5 class="mb-2">Email to get free quote</h5>
-                            <h4 class="text-primary mb-0">info@example.com</h4>
+                            <h4 class="text-primary mb-0">sale&engineer@nt.co.th</h4>
                         </div>
                     </div>
                 </div>
@@ -62,72 +62,44 @@
                         </div>
                         <div class="ps-4">
                             <h5 class="mb-2">Visit our office</h5>
-                            <h4 class="text-primary mb-0">123 Street, NY, USA</h4>
+                            <h4 class="text-primary mb-0">1187 ม.2 ต.ในเมือง อ.เมืองหนองคาย จ.หนองคาย 43000</h4>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="row g-5">
                 <div class="col-lg-6 wow slideInUp" data-wow-delay="0.3s">
-                <form method="POST" action="/contact">
-                    @csrf
-                    <div class="row g-3">
-                        <div class="col-md-6">
-                            <input type="text" name="Ct_username" class="form-control border-0 bg-light px-4" placeholder="Your Name" style="height: 55px">
+                    <form>
+                        <div class="row g-3">
+                            <div class="col-md-6">
+                                <input type="text" class="form-control border-0 bg-light px-4" placeholder="Your Name" style="height: 55px;">
+                            </div>
+                            <div class="col-md-6">
+                                <input type="email" class="form-control border-0 bg-light px-4" placeholder="Your Email" style="height: 55px;">
+                            </div>
+                            <div class="col-12">
+                                <input type="text" class="form-control border-0 bg-light px-4" placeholder="Subject" style="height: 55px;">
+                            </div>
+                            <div class="col-12">
+                                <textarea class="form-control border-0 bg-light px-4 py-3" rows="4" placeholder="Message"></textarea>
+                            </div>
+                            <div class="col-12">
+                                <button class="btn btn-primary w-100 py-3" type="submit">ส่งข้อความหาเรา</button>
+                            </div>
                         </div>
-                        <div class="col-md-6">
-                            <input type="email" name="Ct_email" class="form-control border-0 bg-light px-4" placeholder="Your Email" style="height: 55px">
-                        </div>
-                        <div class="col-12">
-                            <input type="text" name="Ct_title" class="form-control border-0 bg-light px-4" placeholder="Subject" style="height: 55px">
-                        </div>
-                        <div class="col-12">
-                            <textarea name="Ct_detail" class="form-control border-0 bg-light px-4 py-3" rows="4" placeholder="Message"></textarea>
-                        </div>
-                        <div class="col-12">
-                            <button class="btn btn-primary w-100 py-3" type="submit">Send Message</button>
-                        </div>
-                    </div>
-                </form>
+                    </form>
                 </div>
+                <!-- Maps Start -->
                 <div class="col-lg-6 wow slideInUp" data-wow-delay="0.6s">
-                    <iframe class="position-relative rounded w-100 h-100"
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sbd!4v1603794290143!5m2!1sen!2sbd"
-                        frameborder="0" style="min-height: 350px; border:0;" allowfullscreen="" aria-hidden="false"
-                        tabindex="0"></iframe>
+                    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+                    <div id="map" style="height: 400px; border-radius: 10px; margin-top: 20px;"></div>
                 </div>
+                <!-- Maps End -->
             </div>
         </div>
     </div>
     <!-- Contact End -->
 
-
-    <!-- Vendor Start -->
-    <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
-        <div class="container py-5 mb-5">
-            <div class="bg-white">
-                <div class="owl-carousel vendor-carousel">
-                    <img src="frontend/img/vendor-1.jpg" alt="">
-                    <img src="frontend/img/vendor-2.jpg" alt="">
-                    <img src="frontend/img/vendor-3.jpg" alt="">
-                    <img src="frontend/img/vendor-4.jpg" alt="">
-                    <img src="frontend/img/vendor-5.jpg" alt="">
-                    <img src="frontend/img/vendor-6.jpg" alt="">
-                    <img src="frontend/img/vendor-7.jpg" alt="">
-                    <img src="frontend/img/vendor-8.jpg" alt="">
-                    <img src="frontend/img/vendor-9.jpg" alt="">
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Vendor End -->
-
-    <!-- Back to Top -->
-    <a href="#" class="btn btn-lg btn-primary btn-lg-square rounded back-to-top"><i class="bi bi-arrow-up"></i></a>
-
-    <!-- Template Javascript -->
-    <script src="{{asset('frontend/js/main.js')}}"></script>
 </body>
-
 </html>
 @endsection
