422b55997c7aba9e504ae99d65998ad61325ccd6
nattawat <nattawat.wi@kkumail.com>
2023-10-23 22:06:36 +0700

    update

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -9,30 +9,60 @@ use OpenAdmin\Admin\Layout\Column;
 use OpenAdmin\Admin\Layout\Content;
 use OpenAdmin\Admin\Layout\Row;
 
+use App\Models\Person;
+use App\Models\Organization;
+
 class HomeController extends Controller
 {
     public function index(Content $content)
     {
         return $content
-            ->css_file(Admin::asset("open-admin/css/pages/dashboard.css"))
             ->title('Dashboard')
             ->description('Welcome to your custom dashboard!')
-            ->row(Dashboard::title())
             ->row(function (Row $row) {
                 $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::environment());
-                });
-                $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::extensions());
-                });
-                $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::dependencies());
-                });
-                $row->column(12, function (Column $column) {
-                    // Custom content for the dashboard
-                    $column->append(view('admin.charts.bar'));
-                });
-            });
-    }
+                    $personCount = Person::count(); // ตั้งค่าให้ User เป็นชื่อโมเดลของคุณ
+                    $column->append("<div class='card-body'>
+                        <div class='row'>
+                            <div class='col mt-0'>
+                                <h5 class='card-title'>ลูกค้าส่วนบุคคล</h5>
+                            </div>
+                            <div class='col-auto'>
+                                <div class='stat text-primary'>
+                                    <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='feather feather-users align-middle'>
+                                        <path d='M0 0h24v24H0z' stroke='none'/>
+                                        <circle cx='9' cy='7' r='4'/>
+                                        <path d='M9 13.8a7.5 7.5 0 0 1 0-9.6'/>
+                                        <path d='M12 21.4a7.5 7.5 0 0 1 0-9.6'/>
+                                    </svg>
+                                </div>
+                            </div>
+                        </div>
+                        <h1 class='mt-1 mb-3'>$personCount</h1>
+                    </div>");
+                }); 
 
+                $row->column(4, function (Column $column) {
+                    $organizationCount = Organization::count(); // ตั้งค่าให้ User เป็นชื่อโมเดลของคุณ
+                    $column->append("<div class='card-body'>
+                        <div class='row'>
+                            <div class='col mt-0'>
+                                <h5 class='card-title'>ลูกค้าส่วนองค์กร</h5>
+                            </div>
+                            <div class='col-auto'>
+                                <div class='stat text-primary'>
+                                    <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='feather feather-users align-middle'>
+                                        <path d='M0 0h24v24H0z' stroke='none'/>
+                                        <circle cx='9' cy='7' r='4'/>
+                                        <path d='M9 13.8a7.5 7.5 0 0 1 0-9.6'/>
+                                        <path d='M12 21.4a7.5 7.5 0 0 1 0-9.6'/>
+                                    </svg>
+                                </div>
+                            </div>
+                        </div>
+                        <h1 class='mt-1 mb-3'>$organizationCount</h1>
+                    </div>");
+                }); 
+        });
+    }
 }
