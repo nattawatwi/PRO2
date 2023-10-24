ecf53a21ab8a9fbe786efd8e50ae3611c3d0f38a
nattawat <nattawat.wi@kkumail.com>
2023-10-24 23:52:12 +0700

    update

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -251,8 +251,8 @@
                 <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.9s">
                 <div class="position-relative bg-primary rounded h-100 d-flex flex-column align-items-center justify-content-center text-center p-5">
                     <h3 class="text-white mb-3">สอบถามเพิ่มเติม</h3>
-                    <p class="text-white mb-3">Clita ipsum magna kasd rebum at ipsum amet dolor justo dolor est magna stet eirmod</p>
-                    <h2 class="text-white mb-0">+012 345 6789</h2>
+                    <p class="text-white mb-3">สอบถามข้อมูลเพิ่มเติมได้ที่ 1888 หรือ</p>
+                    <h2 class="text-white mb-0">042-411 500</h2>
                 </div>
             </div>
             </div>
